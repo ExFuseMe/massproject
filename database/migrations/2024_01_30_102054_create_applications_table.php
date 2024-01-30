7ebc67ab1e575d5faebb0e97865897ed73ca98fd
ExFuseMe <dn007228@gmail.com>
2024-01-30 15:20:23 +0300

    Create basic Application model. Create application table with columns

---
+++ database/migrations/2024_01_30_102054_create_applications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 256)->nullable(false);
+            $table->string('email')->unique()->nullable(false);
+            //set default to active because of task
+            $table->enum('status', ['active', 'resolved'])->default('active');
+            $table->string('message', 256)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};

145da578a86ac1e0709d35e1a60428b2e1b91c78
ExFuseMe <dn007228@gmail.com>
2024-01-31 16:59:16 +0300

    Add project to branch

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AnswerApplicationRequest;
 use App\Http\Requests\StoreApplicationRequest;
+use App\Mail\ApplicationAnswering;
 use App\Models\Application;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ApplicationController extends Controller
 {
@@ -17,7 +19,22 @@ class ApplicationController extends Controller
         Application::create([
             "name" => $request->name,
             "email" => $request->email,
+            "message" => $request->message
         ]);
         return response()->json(["status"=>200, "text"=>"Request was created successfully"]);
+    }
+    public function update(AnswerApplicationRequest $request, $id){
+        $request->validated();
+        //finding an application by id
+        $application = Application::findOrFail($id);
+        $application->comment = $request->comment;
+        //change status to 'resolved'
+        $application->status = "resolved";
+        $application->save();
+        //send mail to user
+        Mail::send("mail.request.answer", ["comment" => $request->comment], function($message) use($application){
+            $message->to($application->email, $application->name)->subject("Ответ на заявку");
+        });
+        return response()->json(["status" => 200, "text" => "Succesfully answered on request. Email was sent to user"]);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            // set enum to possible modifications
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
     }

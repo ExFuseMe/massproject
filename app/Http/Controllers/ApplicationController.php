c1f92304e3b768184f3c082b763f15b69c9c1b68
ExFuseMe <dn007228@gmail.com>
2024-02-01 10:11:11 +0300

    Add filters to Application model

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -7,23 +7,47 @@ use App\Http\Requests\StoreApplicationRequest;
 use App\Mail\ApplicationAnswering;
 use App\Models\Application;
 use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    public function index(){
-        $applications = Application::all();
+    public function index(Request $request)
+    {
+        $status = $request->query('status');
+        $startDate = $request->query('start_date');
+        $endDate = $request->query('end_date');
+
+        $query = Application::query();
+
+        if ($status) {
+            $query->where('status', $status);
+        }
+
+        if ($startDate && $endDate) {
+            $query->whereBetween('created_at', [Carbon::parse($startDate), Carbon::parse($endDate)]);
+        } elseif ($startDate) {
+            $query->where('created_at', '>=', Carbon::parse($startDate));
+        } elseif ($endDate) {
+            $query->where('created_at', '<=', Carbon::parse($endDate));
+        }
+
+        $applications = $query->get();
+
         return response()->json($applications);
     }
-    public function store(StoreApplicationRequest $request){
+    public function store(StoreApplicationRequest $request)
+    {
         $request->validated();
         Application::create([
             "name" => $request->name,
             "email" => $request->email,
             "message" => $request->message
         ]);
-        return response()->json(["status"=>200, "text"=>"Request was created successfully"]);
+        return response()->json(["status" => 200, "text" => "Request was created successfully"]);
     }
-    public function update(AnswerApplicationRequest $request, $id){
+    public function update(AnswerApplicationRequest $request, $id)
+    {
         $request->validated();
         //finding an application by id
         $application = Application::findOrFail($id);
@@ -32,7 +56,7 @@ class ApplicationController extends Controller
         $application->status = "resolved";
         $application->save();
         //send mail to user
-        Mail::send("mail.request.answer", ["comment" => $request->comment], function($message) use($application){
+        Mail::send("mail.request.answer", ["comment" => $request->comment], function ($message) use ($application) {
             $message->to($application->email, $application->name)->subject("Ответ на заявку");
         });
         return response()->json(["status" => 200, "text" => "Succesfully answered on request. Email was sent to user"]);
